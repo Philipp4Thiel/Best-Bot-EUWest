2c207e8cf5a995866172e86befec55ecd302c5ed
PhilippDesktop <philipp.thiel@hispeed.ch>
2021-04-27 11:03:23 +0200

    everything done except scraper

--- src/main/java/BestBotEuWest/command/commands/ActiveLoop/FFXIVSaleReminderCommand.java
+++ src/main/java/BestBotEuWest/command/commands/ActiveLoop/FFXIVSaleReminderCommand.java
@@ -1,11 +1,20 @@
 package BestBotEuWest.command.commands.ActiveLoop;
 
+import BestBotEuWest.CommandManager;
 import BestBotEuWest.Listener;
 import BestBotEuWest.command.IActiveLoopCommand;
 import net.dv8tion.jda.api.entities.User;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 
 public class FFXIVSaleReminderCommand implements IActiveLoopCommand {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(CommandManager.class);
 
     private static Listener listener;
     private static User owner;
@@ -21,7 +30,13 @@ public class FFXIVSaleReminderCommand implements IActiveLoopCommand {
             owner = listener.getOwner();
         }
 
-        double curPrice = getPrice();
+        double curPrice;
+        try {
+            curPrice = getPrice();
+        } catch (IOException e) {
+            LOGGER.error("couldn't fetch price of FFXIV:" + e.getMessage());
+            return;
+        }
         if (curPrice < lastPrice) {
             owner.openPrivateChannel().complete().sendMessage("price for FFXIV droped from a total of " + lastPrice + " to a total of " + curPrice).queue();
         } else if (curPrice != lastPrice) {
@@ -30,10 +45,18 @@ public class FFXIVSaleReminderCommand implements IActiveLoopCommand {
         lastPrice = curPrice;
     }
 
-    private double getPrice() {
+    private double getPrice() throws IOException {
         double price = 0.0;
 
         //TODO scrap FFXIV shop for prices if prices are lower then last time send msg to owner
+
+        //Story skip shop
+        String url = "https://store.finalfantasyxiv.com/ffxivstore/en-gb/category/0";
+        Document shop = Jsoup.connect(url).get();
+        Elements pageElements = shop.select(".item-card-stretch");
+        System.out.println(">"+pageElements+"<");
+        System.out.println(shop);
+
 
         return Math.round(price * 100) / 100.0;
     }
